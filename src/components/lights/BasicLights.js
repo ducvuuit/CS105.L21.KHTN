bf66ef9a049963d192ea22a36a15548fb3a486c3
CS114.K21.KHTN <62535444+ducvuuit@users.noreply.github.com>
2021-06-11 09:12:10 +0700

    done light

--- src/components/lights/BasicLights.js
+++ src/components/lights/BasicLights.js
@@ -5,14 +5,15 @@ class BasicLights extends Group {
         // Invoke parent Group() constructor with our args
         super(...args);
 
-        const dir = new SpotLight(0xffffff, 1.6, 7, 0.8, 1, 1);
-        const ambi = new AmbientLight(0x404040, 1.32);
-        const hemi = new HemisphereLight(0xffffee, 0x080820, 2.3);
+        //const dir = new SpotLight(0xffffff, 1, 1, 1, 1, 1);
+        const ambi = new AmbientLight(0xffffff, 1);
+        //const hemi = new HemisphereLight(0xffffff, 0xffffff, 1);
 
-        dir.position.set(5, 1, 2);
-        dir.target.position.set(0, 0, 0);
+        //dir.position.set(0, 0, 0);
+        //dir.target.position.set(0, 0, 0);
 
-        this.add(ambi, hemi, dir);
+        //this.add(ambi, hemi, dir);
+        this.add(ambi);
     }
 }
 
